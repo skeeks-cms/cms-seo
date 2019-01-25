350a3cbb1af6a35bcae27d178a5b7dfe2328d010
semenov <semenov@skeeks.com>
2019-01-25 18:10:27 +0300

    fixed

--- src/config/console.php
+++ src/config/console.php
@@ -1,2 +1,17 @@
 <?php
-return [];
+return [
+    'components' => [
+
+        'i18n' => [
+            'translations' => [
+                'skeeks/seo' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@skeeks/cms/seo/messages',
+                    'fileMap' => [
+                        'skeeks/seo' => 'main.php',
+                    ],
+                ]
+            ]
+        ],
+    ],
+];
