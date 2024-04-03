890a86ebfac0c27886096ad84ec9046c426e5811
Semenov <semenov@skeeks.com>
2024-04-03 20:06:03 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -5,32 +5,36 @@ return [
 
     'components' => [
         'seo' => [
-            'class' => 'skeeks\cms\seo\CmsSeoComponent',
+            'class'  => \skeeks\cms\seo\CmsSeoComponent::class,
+            'canUrl' => [
+                'scheme' => \skeeks\cms\seo\CmsSeoComponent::CANURL_DATA_FROM_MAIN_DOMAIN,
+                'host'   => \skeeks\cms\seo\CmsSeoComponent::CANURL_DATA_FROM_MAIN_DOMAIN,
+            ],
         ],
 
         'i18n' => [
             'translations' => [
                 'skeeks/seo' => [
-                    'class' => 'yii\i18n\PhpMessageSource',
+                    'class'    => 'yii\i18n\PhpMessageSource',
                     'basePath' => '@skeeks/cms/seo/messages',
-                    'fileMap' => [
+                    'fileMap'  => [
                         'skeeks/seo' => 'main.php',
                     ],
-                ]
-            ]
+                ],
+            ],
         ],
 
         'urlManager' => [
             'rules' => [
-                'robots.txt' => '/seo/robots/on-request',
+                'robots.txt'  => '/seo/robots/on-request',
                 'sitemap.xml' => '/seo/sitemap/on-request',
-            ]
-        ]
+            ],
+        ],
     ],
 
     'modules' => [
         'seo' => [
             'class' => 'skeeks\cms\seo\CmsSeoModule',
-        ]
-    ]
+        ],
+    ],
 ];
